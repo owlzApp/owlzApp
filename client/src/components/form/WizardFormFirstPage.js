743eea30275b6b35d672dfb00181177a7e67e577
florian lahitte <lahitte.florian@gmail.com>
2019-11-19 08:52:22 -0500

    test phone flag

--- client/src/components/form/WizardFormFirstPage.js
+++ client/src/components/form/WizardFormFirstPage.js
@@ -7,18 +7,42 @@ const WizardFormFirstPage = props => {
   const { handleSubmit } = props;
   return (
     <form onSubmit={handleSubmit}>
-      <Field
-        name="firstName"
-        type="text"
-        component={renderField}
-        label="First Name"
-      />
-      <Field
-        name="lastName"
-        type="text"
-        component={renderField}
-        label="Last Name"
-      />
+      <div className="row">
+        <div className="col m6">
+          <Field
+            name="firstName"
+            type="text"
+            component={renderField}
+            label="First Name"
+          />
+        </div>
+        <div className="col m6">
+          <Field
+            name="lastName"
+            type="text"
+            component={renderField}
+            label="Last Name"
+          />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col m6">
+          <Field
+            name="phone"
+            type="number"
+            component={renderField}
+            label="Phone"
+          />
+        </div>
+        <div className="col m6">
+          <Field
+            name="email"
+            type="email"
+            component={renderField}
+            label="Email"
+          />
+        </div>
+      </div>
       <div>
         <button type="submit" className="next">
           Next
--- client/src/components/form/validate.js
+++ client/src/components/form/validate.js
@@ -11,11 +11,15 @@ const validate = values => {
   } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
     errors.email = "Invalid email address";
   }
+
+  if (!values.phone) {
+    errors.phone = "Required";
+  } else if (!/^(0|[1-9][0-9]{9})$/i.test(values.phone)) {
+    errors.phone = "Invalid phone number, must be 10 digits";
+  }
+
   if (!values.sex) {
     errors.sex = "Required";
-  }
-  if (!values.favoriteColor) {
-    errors.favoriteColor = "Required";
   }
   return errors;
 };
